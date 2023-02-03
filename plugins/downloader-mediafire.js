036eeb07e4516bd204b6cfe0a75ffbcf8b1d9c85
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-03 23:28:29 +0700

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,21 +1,23 @@
-const { mediafiredl } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let caption = `
-*💌 Name:* ${filename}
-*📊 Size:* ${filesizeH}
-*🗂️ Extension:* ${ext}
-*📨 Uploaded:* ${aploud}
-`.trim()
-    m.reply(caption)
-    await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+const { mediafireDl } = require('../lib/mediafire.js')
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+if (!text) return m.reply(`Kirim perintah ${usedPrefix + command} *link mediafire*`)
+if (!args[0].includes('mediafire.com')) return m.reply(error.linkmf)
+let mdjon = args.join(' ')
+res = await mediafireDl(mdjon)
+result = `「 *ᴍᴇᴅɪᴀꜰɪʀᴇ ᴅᴏᴡɴʟᴏᴀᴅ* 」
+*Data Berhasil Didapatkan!*
+*💌Nama* : ${res[0].nama}
+*📊Size File* : ${res[0].size}
+*🗂️Media Link* : ${res[0].link}
+*Powered By* : Saxia`Botz
+_Tunggu Proses Upload Media Memerlukan Waktu Beberapa Detik_`
+m.reply(result)
+//await sleep(100)
+conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
 }
 handler.help = ['mediafire'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(mediafire|mf)$/i
 
 handler.limit = true
-
 module.exports = handler
