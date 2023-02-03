53be090c2b7222e4550ce2bf2c2ec5416b8aef2b
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-03 15:42:20 +0700

    Update _anti212.js

--- plugins/_anti212.js
+++ plugins/_anti212.js
@@ -13,5 +13,26 @@ handler.before = async function (m) {
     await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
    }
  }
+ if (m.sender.startsWith('90' || '90')) {
+   	global.db.data.users[m.sender].banned = true
+    await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
+   }
+ }
+ if (m.sender.startsWith('91' || '91')) {
+   	global.db.data.users[m.sender].banned = true
+    await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
+   }
+ }
+ if (m.sender.startsWith('92' || '92')) {
+   	global.db.data.users[m.sender].banned = true
+    await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
+   }
+ }
+ if (m.sender.startsWith('55' || '55')) {
+   	global.db.data.users[m.sender].banned = true
+    await conn.updateBlockStatus(m.sender, "block") // hapus kalau ga mau pasang
+   }
+ }
+
 
 module.exports = handler
