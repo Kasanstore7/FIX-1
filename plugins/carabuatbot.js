7c73505948b6298a1d4e63f13033157f3181bc80
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-03 16:08:34 +0700

    Update carabuatbot.js

--- plugins/carabuatbot.js
+++ plugins/carabuatbot.js
@@ -1,30 +1,30 @@
 let fs = require('fs')
 let handler = async (m, { conn }) => {
-let jarot = 'https://telegra.ph/file/947607f0d10667f4f8e0f.jpg'
+let jarot = 'https://telegra.ph/file/b993bdac4ba1d327da3be.jpg'
 let os = `┌─── 〔 TUTORIAL JADIBOT 〕 ───
-✘.SILAHKAN KE BERANDA WHATSAPP 
-✘.CARI TULISAN PERANGKAT TAUTAN 
-✘.PENCET PERANGKAT TAUTAN 
-✘.SILAHKAN MINTA KODE QR KE WA BOT
-✘.KALO UDAH DAPET SILAHKAN SCAN
-✘.TUNGGU SAMPAI PERANGKAT MASUK
-✘.KALO UDAH MASUK KETIK LAGI JADIBOT
-✘.CONNECT 
+✧.SILAHKAN KE BERANDA WHATSAPP 
+✧.CARI TULISAN PERANGKAT TAUTAN 
+✧.PENCET PERANGKAT TAUTAN 
+✧.SILAHKAN MINTA KODE QR KE WA BOT
+✧.KALO UDAH DAPET SILAHKAN SCAN
+✧.TUNGGU SAMPAI PERANGKAT MASUK
+✧.KALO UDAH MASUK KETIK LAGI JADIBOT
+✧.CONNECT 
 
-❍ TUTOR DAPETIN KODE QR ❍
+❍ TUTOR DAPETIN KODE QR ?? ❍
 
 ✘.JOIN GROUP BOT
-https://chat.whatsapp.com/KGuKY4wuKHS8sj6K5IHjFG
+https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz
 ✘.JANGAN LUPA TAG OWNER
 ✘.BIAR DI ACC KALO UDAH DI ACC
 ✘.KETIK .jadibot DI NOMER BOT INI
 ✘.SILAHKAN SCAN  
 ✘.JANGAN LUPA DI BACA SEMUA
 `
-  conn.send3ButtonImg(m.chat, jarot, os, "📮 Silahkan pilih Salah Satu dibawah ini", 'Sosialmedia', '.sosialmedia', 'Sewa', '.sewa', 'Menu', '.menu', m) 
+  conn.send3ButtonImg(m.chat, jarot, os, "📮 Silahkan pilih dibawah ini Dan Jangan Spam", 'Sosialmedia', '.sosialmedia', 'Sewa', '.sewa', 'Menu', '.menu', m) 
 }
-handler.help = ['carabuatbot']
+handler.help = ['tutorjdibot']
 handler.tags = ['main']
-handler.command = /^(cbb|carabuatbot)$/i
+handler.command = /^(tjd|tutorjdibot)$/i
 
 module.exports = handler
