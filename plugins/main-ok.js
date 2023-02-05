6f87b652d42b0c74f93c77576271ad6381c0daeb
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-05 15:23:48 +0700

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,17 +1,9 @@
-let fs = require('fs')
-let handler = async (m, { conn }) => {
-//FAKEREPLY KONTAK
- const fcon = {
-	 key:
-	 { fromMe: false,
-	 participant: `0@s.whatsapp.net`, ...(m.chat ? 
-	 { remoteJid: "status@broadcast" } : {}) },
-	 message: { "contactMessage": { "title":"sri","h": `haloo`, 'jpegThumbnail': fs.readFileSync('./image/3SBot.jpg')}}
-	}
-conn.reply(m.chat, diskusi, fcon) 
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f0b1c19aca4215ffb4ac7.jpg', m, { packname: "wa.me/6283111136642", author: "Saxia Botz\nig @m.rxcl_" })
 }
-handler.help = ['gc3']
-handler.tags = ['info']
-handler.command = /^(gc3)$/i
+
+handler.customPrefix = /^(ok|okey|otey|okeh)$/i
+handler.command = new RegExp
 
 module.exports = handler
