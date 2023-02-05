a54af22448828ffd4ba70c68d4b9ffcda95166b4
Mshunone <100399123+Mshunone@users.noreply.github.com>
2023-02-05 15:38:28 +0700

    Update downloader-igstalk.js

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args || !args[0]) throw `Gunakan format ${usedPrefix}${command} [username]
-Contoh: ${usedPrefix}${command} 𝐉𝐚𝐫𝐨𝐭
+Contoh: ${usedPrefix}${command} @m.rxcl_ Jan lupa folow sekalian :v
 `.trim()
   let res = await igstalk(args[0])
   let json = JSON.parse(JSON.stringify(res))
